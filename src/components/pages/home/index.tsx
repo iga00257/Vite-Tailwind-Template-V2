e90b375f157d4b30a558818093e4f4b9838166a1
Derrick <iga00257@gmail.com>
2025-06-06 00:16:45 +0800

    chore: remove unused code

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,15 +1,6 @@
-import { fetchPokemon } from '@/api/fetchPokemon';
-import { useQuery } from '@tanstack/react-query';
 import { t } from 'i18next';
 
 const HomePageComponent = () => {
-  const { data } = useQuery({
-    queryKey: ['test'],
-    queryFn: () => fetchPokemon({ name: 'bulbasaur' }),
-  });
-
-  console.log('data', data);
-
   return (
     <div className='flex flex-col w-full items-center shrink-0 self-center'>
       <p className='textSize-9 text-green-500'>{t('Common_Welcome_Home', { lng: 'en' })}</p>
