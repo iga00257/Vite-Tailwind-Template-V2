2916025b8bdca322657147770eba2216216da500
derrick <iga00257@gmail.com>
2025-06-06 14:19:26 +0800

    feat: update setting & env

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,16 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_DEPLOY_ENV: string;
-  }
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DEPLOY_ENV: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.svg?react' {
+  import React from 'react';
+  const SVGComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVGComponent;
 }
